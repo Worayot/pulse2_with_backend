2bff6deccbbcfa101171c8bdaf880f661d4cc6ad
Worayot-MAC <worayot@WorayotMAC.local>
2025-03-23 21:57:49 +0700

    Before new feature was implemented

--- lib/services/server_url.dart
+++ lib/services/server_url.dart
@@ -1,4 +1,4 @@
 final String baseUrl =
-    'https://1c22-2405-9800-ba00-ca0-14a6-f0a6-dced-c4c8.ngrok-free.app';
+    'https://694f-2405-9800-ba00-ca0-5475-1028-70a-54e9.ngrok-free.app';
 
 // final String baseUrl = 'http://localhost:8000';
