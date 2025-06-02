ca238d77f0955102cb2a5f9fb66b479105389980
siwasit <siwasit2546@gmail.com>
2025-06-03 01:22:22 +0700

    Configured android folder

--- lib/services/export_services.dart
+++ lib/services/export_services.dart
@@ -11,9 +11,6 @@ import 'package:tuh_mews/services/url.dart';
 
 class ExportServices {
   Future<Map<int, String>> export(List<String> patientIds) async {
-    // final _storage = FlutterSecureStorage();
-    // String? idToken = await _storage.read(key: 'id_token');
-
     String? idToken = await SessionService().getIdToken();
 
     if (idToken == null) {
@@ -35,55 +32,71 @@ class ExportServices {
       );
 
       if (response.statusCode == 200) {
-        return await _saveAndOpenFile(response.bodyBytes, response.statusCode);
+        return await _saveAndOpenFile(response.bodyBytes);
       } else {
         return {401: "Token expired"};
       }
     } catch (e) {
-      // print("Error getting note: $e");
       return {500: "Internal Server Error: $e"};
     }
   }
 
-  Future<Map<int, String>> _saveAndOpenFile(
-    List<int> excelData,
-    int statusCode,
-  ) async {
-    var status = await Permission.storage.request();
-    if (status.isGranted) {
-      try {
-        Directory? directory;
-        if (Platform.isAndroid) {
-          directory = await getExternalStorageDirectory();
-        } else if (Platform.isIOS) {
-          directory = await getApplicationDocumentsDirectory();
+  Future<Map<int, String>> _saveAndOpenFile(List<int> excelData) async {
+    try {
+      Directory? directory;
+
+      if (Platform.isAndroid) {
+        bool permissionGranted = await _requestStoragePermissions();
+        if (!permissionGranted) {
+          return {403: "Forbidden: Storage permission denied."};
         }
 
+        final directories = await getExternalStorageDirectories(
+          type: StorageDirectory.downloads,
+        );
+        directory = directories?.first;
         if (directory == null) {
-          // print('Error: Could not get directory.');
           return {500: "Internal Server Error: Could not get directory."};
         }
-
-        final uuid = Uuid();
-        final uniqueFileName = 'all_patients_report_${uuid.v4()}.xlsx';
-        final filePath = '${directory.path}/$uniqueFileName';
-        final file = File(filePath);
-        await file.writeAsBytes(excelData);
-
-        final result = await OpenFile.open(filePath);
-        // print('File opened: ${result.message}');
-        if (result.type == ResultType.done) {
-          return {200: "File saved and opened successfully."};
-        } else {
-          return {500: "Error opening file: ${result.message}"};
-        }
-      } catch (e) {
-        // print('Error saving/opening file: $e');
-        return {500: "Internal Server Error: $e"};
+      } else if (Platform.isIOS) {
+        directory = await getApplicationDocumentsDirectory();
+      } else {
+        return {500: "Unsupported platform."};
       }
-    } else {
-      // print('Storage permission denied.');
-      return {403: "Forbidden: Storage permission denied."};
+
+      final uuid = Uuid();
+      final uniqueFileName = 'all_patients_report_${uuid.v4()}.xlsx';
+      final filePath = '${directory.path}/$uniqueFileName';
+      final file = File(filePath);
+      await file.writeAsBytes(excelData);
+
+      final result = await OpenFile.open(filePath);
+      if (result.type == ResultType.done) {
+        return {200: "File saved and opened successfully."};
+      } else {
+        return {500: "Error opening file: ${result.message}"};
+      }
+    } catch (e) {
+      return {500: "Internal Server Error: $e"};
     }
+  }
+
+  Future<bool> _requestStoragePermissions() async {
+    if (Platform.isAndroid) {
+      // For Android 11+ request MANAGE_EXTERNAL_STORAGE permission
+      if (await Permission.manageExternalStorage.isGranted) {
+        return true;
+      }
+      final status = await Permission.manageExternalStorage.request();
+      if (status.isGranted) {
+        return true;
+      } else {
+        // Open app settings for manual enablement
+        await openAppSettings();
+        return false;
+      }
+    }
+    // iOS does not need special permission for documents directory
+    return true;
   }
 }
