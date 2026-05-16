128d51d0a244cea158211b643360ddd08fe29990
Worayot <worayotkhaowoat@gmail.com>
2026-05-16 20:03:12 +0700

    Fix login screen scrolling

--- lib/authentication/login.dart
+++ lib/authentication/login.dart
@@ -187,152 +187,161 @@ class _LoginPageState extends ConsumerState<LoginPage> {
     final size = MediaQuery.of(context).size;
 
     return Scaffold(
-      // resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            return SingleChildScrollView(
-              child: Stack(
-                children: [
-                  Positioned(
-                    top: MediaQuery.of(context).padding.top + 20,
-                    left: 0,
-                    child: SizedBox(width: size.width / 3, child: Image.asset('assets/images/img_login_top.png', fit: BoxFit.contain)),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(24.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        SizedBox(height: size.height * 0.1),
-                        Align(alignment: Alignment.center, child: Text('MEWS', style: TextStyle(fontSize: size.height * 0.095, fontWeight: FontWeight.bold, color: Colors.black))),
-                        Card(
-                          elevation: 0,
-                          shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                          color: const Color(0xffE0EAFF),
-                          child: Padding(
-                            padding: EdgeInsets.symmetric(vertical: size.width / 10, horizontal: size.width / 15),
-                            child: Form(
-                              key: _formKey,
-                              child: Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      ToggleButtons(
-                                        isSelected: [_selectedLanguageIndex == 0, _selectedLanguageIndex == 1],
-                                        onPressed: _toggleLanguage,
-                                        borderRadius: BorderRadius.circular(10),
-                                        selectedColor: Colors.white,
-                                        fillColor: const Color(0xff1225A4),
-                                        color: Colors.black,
-                                        constraints: const BoxConstraints(minWidth: 70, minHeight: 36),
-                                        children: const [Text('English'), Text('ไทย')],
-                                      ),
-                                    ],
-                                  ),
-                                  const SizedBox(height: 20),
-                                  Row(children: [Text("nurseID".tr(), style: const TextStyle(fontSize: 16), textAlign: TextAlign.left)]),
-                                  TextFormField(
-                                    controller: _nurseIDController,
-                                    decoration: InputDecoration(
-                                      hintText: "\t\t${"fillInNurseID".tr()}",
-                                      border: const UnderlineInputBorder(),
-                                      floatingLabelBehavior: FloatingLabelBehavior.always,
-                                      errorBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                                      focusedErrorBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Colors.red, width: 2)),
-                                    ),
-                                    keyboardType: TextInputType.numberWithOptions(decimal: false),
-                                    validator: (value) {
-                                      if (value == null || value.trim().isEmpty) {
-                                        return "plsEnterNurseID".tr(); // Use .tr() if localized
-                                      }
-                                      if (!RegExp(r'^\d+$').hasMatch(value)) {
-                                        return "nurseIDMustBeANumber".tr();
-                                      }
-                                      return null; // No error
-                                    },
-                                  ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: MediaQuery.of(context).padding.top + 20,
+              left: 0,
+              child: SizedBox(width: size.width / 3, child: Image.asset('assets/images/img_login_top.png', fit: BoxFit.contain)),
+            ),
 
-                                  SizedBox(height: size.height / 50),
-                                  Row(children: [Text("password".tr(), style: const TextStyle(fontSize: 16), textAlign: TextAlign.left)]),
-                                  TextFormField(
-                                    controller: _passwordController,
-                                    decoration: InputDecoration(
-                                      hintText: "\t\t${"fillInPassword".tr()}",
-                                      border: const UnderlineInputBorder(),
-                                      floatingLabelBehavior: FloatingLabelBehavior.always,
-                                      errorBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Colors.red)),
-                                      focusedErrorBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Colors.red, width: 2)),
-                                      suffixIcon: IconButton(
-                                        icon: Icon(obscurePassword ? Icons.visibility_off : Icons.visibility),
-                                        onPressed: () {
-                                          setState(() {
-                                            obscurePassword = !obscurePassword;
-                                          });
-                                        },
-                                      ),
-                                    ),
-                                    keyboardType: TextInputType.visiblePassword,
-                                    obscureText: obscurePassword,
-                                    validator: (value) {
-                                      if (value == null || value.trim().isEmpty) {
-                                        return "plsEnterPassword".tr();
-                                      }
-                                      return null;
-                                    },
-                                  ),
-                                  const Gap(16),
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.end,
-                                    children: [
-                                      Checkbox(
-                                        checkColor: Colors.white,
-                                        focusColor: const Color(0xff1225A4),
-                                        activeColor: const Color(0xff1225A4),
-                                        value: rememberMe,
-                                        onChanged: (value) {
-                                          if (value == null) return;
-                                          setState(() {
-                                            rememberMe = value;
-                                            secureStorage.write(key: 'rememberMe', value: (value).toString());
-                                          });
-                                        },
-                                      ),
-                                      Text("rememberMe?".tr()),
-                                    ],
-                                  ),
-                                  const Gap(16),
-                                  SizedBox(
-                                    width: size.width * 0.5,
-                                    height: size.height * 0.07,
-                                    child: ElevatedButton(
-                                      style: ElevatedButton.styleFrom(
-                                        backgroundColor: const Color(0xff1225A4),
-                                        padding: const EdgeInsets.symmetric(vertical: 0),
-                                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
-                                        elevation: 2,
-                                      ),
-                                      onPressed: _login,
-                                      child:
-                                          isLoading
-                                              ? Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white)))
-                                              : Text("login".tr(), style: const TextStyle(fontSize: 22, color: Colors.white)),
-                                    ),
-                                  ),
-                                ],
+            ListView(
+              keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+              physics: MediaQuery.of(context).viewInsets.bottom > 0 ? const ClampingScrollPhysics() : const NeverScrollableScrollPhysics(),
+              padding: const EdgeInsets.all(24.0),
+              children: [
+                SizedBox(height: size.height * 0.1),
+
+                Align(alignment: Alignment.center, child: Text('MEWS', style: TextStyle(fontSize: size.height * 0.095, fontWeight: FontWeight.bold, color: Colors.black))),
+
+                Card(
+                  elevation: 0,
+                  shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
+                  color: const Color(0xffE0EAFF),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(vertical: size.width / 10, horizontal: size.width / 15),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              ToggleButtons(
+                                isSelected: [_selectedLanguageIndex == 0, _selectedLanguageIndex == 1],
+                                onPressed: _toggleLanguage,
+                                borderRadius: BorderRadius.circular(10),
+                                selectedColor: Colors.white,
+                                fillColor: const Color(0xff1225A4),
+                                color: Colors.black,
+                                constraints: const BoxConstraints(minWidth: 70, minHeight: 36),
+                                children: const [Text('English'), Text('ไทย')],
+                              ),
+                            ],
+                          ),
+
+                          const SizedBox(height: 20),
+
+                          Row(children: [Text("nurseID".tr(), style: const TextStyle(fontSize: 16))]),
+
+                          TextFormField(
+                            controller: _nurseIDController,
+                            decoration: InputDecoration(
+                              hintText: "\t\t${"fillInNurseID".tr()}",
+                              border: const UnderlineInputBorder(),
+                              floatingLabelBehavior: FloatingLabelBehavior.always,
+                              errorBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Colors.red)),
+                              focusedErrorBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Colors.red, width: 2)),
+                            ),
+                            keyboardType: const TextInputType.numberWithOptions(decimal: false),
+                            validator: (value) {
+                              if (value == null || value.trim().isEmpty) {
+                                return "plsEnterNurseID".tr();
+                              }
+
+                              if (!RegExp(r'^\d+$').hasMatch(value)) {
+                                return "nurseIDMustBeANumber".tr();
+                              }
+
+                              return null;
+                            },
+                          ),
+
+                          SizedBox(height: size.height / 50),
+
+                          Row(children: [Text("password".tr(), style: const TextStyle(fontSize: 16))]),
+
+                          TextFormField(
+                            controller: _passwordController,
+                            decoration: InputDecoration(
+                              hintText: "\t\t${"fillInPassword".tr()}",
+                              border: const UnderlineInputBorder(),
+                              floatingLabelBehavior: FloatingLabelBehavior.always,
+                              errorBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Colors.red)),
+                              focusedErrorBorder: const UnderlineInputBorder(borderSide: BorderSide(color: Colors.red, width: 2)),
+                              suffixIcon: IconButton(
+                                icon: Icon(obscurePassword ? Icons.visibility_off : Icons.visibility),
+                                onPressed: () {
+                                  setState(() {
+                                    obscurePassword = !obscurePassword;
+                                  });
+                                },
                               ),
                             ),
+                            keyboardType: TextInputType.visiblePassword,
+                            obscureText: obscurePassword,
+                            validator: (value) {
+                              if (value == null || value.trim().isEmpty) {
+                                return "plsEnterPassword".tr();
+                              }
+
+                              return null;
+                            },
                           ),
-                        ),
-                      ],
+
+                          const Gap(16),
+
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Checkbox(
+                                checkColor: Colors.white,
+                                focusColor: const Color(0xff1225A4),
+                                activeColor: const Color(0xff1225A4),
+                                value: rememberMe,
+                                onChanged: (value) {
+                                  if (value == null) return;
+
+                                  setState(() {
+                                    rememberMe = value;
+
+                                    secureStorage.write(key: 'rememberMe', value: value.toString());
+                                  });
+                                },
+                              ),
+                              Text("rememberMe?".tr()),
+                            ],
+                          ),
+
+                          const Gap(16),
+
+                          SizedBox(
+                            width: size.width * 0.5,
+                            height: size.height * 0.07,
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: const Color(0xff1225A4),
+                                padding: const EdgeInsets.symmetric(vertical: 0),
+                                shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                                elevation: 2,
+                              ),
+                              onPressed: _login,
+                              child:
+                                  isLoading
+                                      ? const Center(child: CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.white)))
+                                      : Text("login".tr(), style: const TextStyle(fontSize: 22, color: Colors.white)),
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
                   ),
-                ],
-              ),
-            );
-          },
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
